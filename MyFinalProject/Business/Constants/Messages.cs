fa926025273220ec13c8f380e41e302ecc5a28e4
iremmakar <akarr.iirem@gmail.com>
2023-08-21 16:52:44 +0300

    backend

---
+++ MyFinalProject/Business/Constants/Messages.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+
+        public static string MaintenanceTime = "";
+        public static string ProductsListed = "";
+
+        public static string ProductCountOfCategoryError = "Kategori sayısı 15 den fazla olamaz";
+
+        public static string ProductNameAlreadyExist = "Product adı mevcut";
+
+        public static string AuthorizationDenied ="Yetkiniz bulunmamaktadır";
+    }
+}
---
+++ MyFinalProject/WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("getall")]
+        
+        public IActionResult GetAll()
+        {
+            var result = _categoryService.GetAll();
+
+
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+
+        }
+    }
+}
